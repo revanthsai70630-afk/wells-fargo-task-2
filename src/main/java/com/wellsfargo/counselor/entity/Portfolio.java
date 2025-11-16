e4a2ebfc64cb0f8d1c7a06fa0ccf888d7ba6d2fb
revanthsai70630-afk <238689582+revanthsai70630-afk@users.noreply.github.com>
2025-11-16 23:12:10 +0530

    Create Portfolio.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,53 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client, Date creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
